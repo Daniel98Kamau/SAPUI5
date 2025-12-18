1a1ca32c17aad7338d73e8269b42b5dd4998e509
Meka <danielkamaunjoroge@gmail.com>
2025-12-18 09:12:46 +0300

    First commit

---
+++ Hello_App/helloui5/webapp/controller/View1.controller.ts
@@ -0,0 +1,15 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace helloui5.controller
+ */
+export default class View1 extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+    onPress(): void {
+    alert("Button clicked!");
+    }
+}
