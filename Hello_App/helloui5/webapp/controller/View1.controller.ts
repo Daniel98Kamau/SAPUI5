9706aae79ecd2f993f76cf1eb4555a86ae897ccf
Meka <danielkamaunjoroge@gmail.com>
2026-02-07 03:40:00 +0300

    custom header in a page

--- Hello_App/helloui5/webapp/controller/View1.controller.ts
+++ Hello_App/helloui5/webapp/controller/View1.controller.ts
@@ -1,6 +1,7 @@
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import Input from "sap/m/Input";
 
 /**
  * @namespace helloui5.controller
